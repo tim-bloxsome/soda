c8442d4a5deef1f9daddbdacecfc6f8df294683a
tim-bloxsome <44049747+tim-bloxsome@users.noreply.github.com>
2019-02-10 11:09:40 +1000

    massive restructure

--- soda/wsgi.py
+++ soda/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Soda project.
+WSGI config for soda project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Soda.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soda.settings')
 
 application = get_wsgi_application()
